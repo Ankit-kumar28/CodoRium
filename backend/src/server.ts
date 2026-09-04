9fc359edc7d7d507e579ec6bf6773951ffbe6470
Ankit-kumar28 <ankitk123907@gmail.com>
2026-09-04 14:05:31 +0530

    update server for deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ async function startServer() {
   );
 
   try {
-    // await verifyEmailConnection();  it taking timeout 
+    // await verifyEmailConnection();  it taking timeout m
 
     app.listen(
       env.PORT,
